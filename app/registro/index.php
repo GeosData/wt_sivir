ac26f138bb78a567d4572d211d1e33adc2ee02d1
JosueR7 <rojo.jr72@gmail.com>
2017-04-19 13:51:13 -0500

    Inicio app de Registro, creacion de formulario

---
+++ app/registro/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Registro</title>
+</head>
+<body>
+	<h1>Registro</h1>
+	<div id="form-box">
+		<form action="#" method="post">
+			<input type="text" name="user" placeholder="Usuario" required>
+			<input type="password" name="pass" placeholder="Contraseña" required>
+			<input type="email" name="email" placeholder="Correo Electrónico" required>
+			<input type="text" name="name" placeholder="Nombre" required>
+			<input type="text" name="lastname" placeholder="Apellidos" required>
+			<input type="submit" value="Registrar">
+		</form>
+	</div>
+</body>
+</html>
