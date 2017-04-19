3fa3f794a153ce543167c2b8418ae23357c69a30
JosueR7 <rojo.jr72@gmail.com>
2017-04-19 15:56:09 -0500

    mejora de seguridad en insercion de datos

--- app/registro/core/registro.php
+++ app/registro/core/registro.php
@@ -4,17 +4,24 @@ require_once 'conect.php';
 
 $conect = new Conect();
 
-$user = $_POST['user'];
-$pass = md5($_POST['pass']);
-$email = $_POST['email'];
-$name = $_POST['name'];
-$lastname = $_POST['lastname'];
+if ($_POST != null) {
+	$user = $_POST['user'];
+	$pass = md5($_POST['pass']);
+	$email = $_POST['email'];
+	$name = $_POST['name'];
+	$lastname = $_POST['lastname'];
 
-$sql = "INSERT INTO usuario (idusuario,usuario,clave,correo,nombre,apellidos) VALUES('','$user','$pass','$email','$name','$lastname')";
+	$sql = "INSERT INTO usuario (idusuario,usuario,clave,correo,nombre,apellidos) VALUES('','$user','$pass','$email','$name','$lastname')";
 
-$dato = $conect->prepare($sql);
-$dato->execute();
-//$this->id = $conexion->lastInsertId();
+	$dato = $conect->prepare($sql);
+	$dato->execute();
+
+	header("location:../");
+}else {
+	echo 'no se ingreso ningun dato';# code...
+}
+
+
 
 
 ?>
