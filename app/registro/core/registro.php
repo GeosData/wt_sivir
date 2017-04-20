14d76f997b739ed1fe20949a39e965f2a6436de0
JosueR7 <rojo.jr72@gmail.com>
2017-04-19 19:47:14 -0500

    Validacion de registros ya existentes

--- app/registro/core/registro.php
+++ app/registro/core/registro.php
@@ -4,6 +4,7 @@ require_once '../../../core/conect.php';
 
 $conect = new Conect();
 
+
 if ($_POST != null) {
 	$user = $_POST['user'];
 	$pass = md5($_POST['pass']);
@@ -11,10 +12,32 @@ if ($_POST != null) {
 	$name = $_POST['name'];
 	$lastname = $_POST['lastname'];
 
-	$sql = "INSERT INTO usuario (idusuario,usuario,clave,correo,nombre,apellidos) VALUES('','$user','$pass','$email','$name','$lastname')";
+	$st = $conect->prepare("SELECT * FROM usuario WHERE usuario=:usuario"); 
+	$st->bindParam("usuario", $user,PDO::PARAM_STR);
+	$st->execute();
+	$count=$st->rowCount();
+ 
+ 	if($count<1){
 
-	$dato = $conect->prepare($sql);
-	$dato->execute();
+ 		$st1 = $conect->prepare("SELECT * FROM usuario WHERE correo=:email"); 
+		$st1->bindParam("email", $email,PDO::PARAM_STR);
+		$st1->execute();
+		$count1=$st1->rowCount();
+ 		
+ 		if ($count1<1) {
+ 			$sql = "INSERT INTO usuario (idusuario,usuario,clave,correo,nombre,apellidos) VALUES('','$user','$pass','$email','$name','$lastname')";
+
+			$dato = $conect->prepare($sql);
+			$dato->execute();
+		}else{
+			echo "Esta direccion de correo ya esta registrado, intente con otra";
+		}
+	}else {
+		echo "Usuario Ya existe, intente con otro";
+	}
+
+	echo "Registro Exitoso";
+
 
 	header("location:../");
 }else {
